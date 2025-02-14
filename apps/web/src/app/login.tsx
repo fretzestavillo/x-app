1531c009763d8ff86fae2e281f6e6700a4be4c3a
fretzestavillo <estavillofretz@gmail.com>
2025-02-14 20:27:25 +0800

    add fire base authentication

---
+++ apps/web/src/app/login.tsx
@@ -0,0 +1,33 @@
+import Button from '@mui/material/Button';
+
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { firebaseConfig } from './firebase-config';
+
+export function Login() {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, new GoogleAuthProvider());
+      console.log(result);
+    } catch (error) {
+      console.error('Login Error:', error);
+    }
+  };
+
+  return (
+    <>
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        sx={{ marginTop: 2 }}
+        onClick={handleLogin}
+      >
+        Login with Google
+      </Button>
+    </>
+  );
+}
