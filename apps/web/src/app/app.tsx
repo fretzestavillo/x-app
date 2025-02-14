6209ca3a894868680cda3265e8827498ee96ceb5
fretzestavillo <estavillofretz@gmail.com>
2025-02-14 20:27:50 +0800

    add react router

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,16 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './login';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="web" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
