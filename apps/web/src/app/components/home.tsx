7c0da8e48a4efb835a11ead2148452ec436e175f
fretzestavillo <estavillofretz@gmail.com>
2025-02-17 12:11:38 +0800

    retrieve local storage

--- apps/web/src/app/components/home.tsx
+++ apps/web/src/app/components/home.tsx
@@ -1,4 +1,13 @@
 export function Home() {
+  const userData = localStorage.getItem('user');
+
+  if (userData) {
+    const parsedUserData = JSON.parse(userData);
+    console.log('User Data:', parsedUserData);
+  } else {
+    console.log('No user data found in local storage.');
+  }
+
   return (
     <>
       <h1>Home</h1>
