e988b821c773a12f946dd7a06236688fec5118bc
fretzestavillo <estavillofretz@gmail.com>
2025-02-17 12:04:52 +0800

    rename endpoint

--- apps/web/src/app/components/signin.tsx
+++ apps/web/src/app/components/signin.tsx
@@ -41,7 +41,7 @@ export function SignIn() {
 
   const onSubmit = async (data: any) => {
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('http://localhost:3000/api/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
