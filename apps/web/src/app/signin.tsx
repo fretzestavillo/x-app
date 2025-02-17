aa28bed420536b0c37c18c71e65b009e847b303c
fretzestavillo <estavillofretz@gmail.com>
2025-02-17 10:54:49 +0800

    add sign in component

---
+++ apps/web/src/app/signin.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Box,
+  Snackbar,
+  Alert,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Invalid email format')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
+
+export function SignIn() {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>(
+    'success'
+  );
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const result = await response.json();
+      setSnackbarMessage('Login Successful!');
+      setSnackbarSeverity('success');
+      setOpenSnackbar(true);
+      console.log(result);
+      //   navigate('/home');
+    } catch (error) {
+      setSnackbarMessage('Login Failed! Please check your credentials.');
+      setSnackbarSeverity('error');
+      setOpenSnackbar(true);
+
+      console.error('Error:', error);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        sx={{
+          width: '100%',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: 1300,
+        }}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbarSeverity}
+          sx={{ width: '100%' }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+
+      <Container maxWidth="sm">
+        <Box sx={{ mt: 10, p: 3, borderRadius: 2, boxShadow: 2 }}>
+          <Typography variant="h5" gutterBottom>
+            Login Form
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              fullWidth
+              type="email"
+              label="Email Address"
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              type="password"
+              label="Password"
+              {...register('password')}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              margin="normal"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              type="submit"
+              sx={{ mt: 2 }}
+            >
+              Login
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
+}
