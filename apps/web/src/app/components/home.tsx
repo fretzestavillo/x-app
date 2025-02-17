89492d21aa20dff5ac2adc2ffed34f1aa3e2e4b2
fretzestavillo <estavillofretz@gmail.com>
2025-02-17 12:38:54 +0800

    access local storage

--- apps/web/src/app/components/home.tsx
+++ apps/web/src/app/components/home.tsx
@@ -1,16 +1,36 @@
-export function Home() {
-  const userData = localStorage.getItem('user');
+import { Avatar } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { useEffect, useState } from 'react';
 
-  if (userData) {
-    const parsedUserData = JSON.parse(userData);
-    console.log('User Data:', parsedUserData);
-  } else {
-    console.log('No user data found in local storage.');
-  }
+export function Home() {
+  const [id, setId] = useState('');
+  const [accessToken, setAccessToken] = useState('');
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const userData = localStorage.getItem('user');
+
+    if (userData) {
+      const parsedUserData = JSON.parse(userData);
+      console.log('User Data:', parsedUserData);
+      setId(parsedUserData.id);
+      setAccessToken(parsedUserData.accessToken);
+      setName(parsedUserData.name);
+    } else {
+      console.log('No user data found in local storage.');
+    }
+  }, []);
+
+  const nameParts = name.split(' ');
+  const initials = nameParts[0][0] + nameParts[nameParts.length - 1][0];
 
   return (
     <>
-      <h1>Home</h1>
+      <div>
+        <Avatar sx={{ bgcolor: red[500], cursor: 'pointer' }}>
+          {initials}
+        </Avatar>
+      </div>
     </>
   );
 }
