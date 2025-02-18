edc89d55fe527077975063239e24e0f4db17241d
fretzestavillo <estavillofretz@gmail.com>
2025-02-18 17:24:00 +0800

    add serve static into app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,10 +6,16 @@ import { ConfigModule } from '@nestjs/config';
 import { PostEntity } from './post/post.entity';
 import { PostModule } from './post/post.module';
 import { ProfilePicEntity } from './post/profile.pic.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'uploads'), // Going up three levels to access 'uploads'
+      serveRoot: '/uploads', // This is the URL path to access your files
+    }),
 
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as 'postgres',
