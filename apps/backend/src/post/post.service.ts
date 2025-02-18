0830dc873bf44037a2acbdc4e0830d515e89d5e0
fretzestavillo <estavillofretz@gmail.com>
2025-02-18 14:46:31 +0800

    add method to save file path in database

--- apps/backend/src/post/post.service.ts
+++ apps/backend/src/post/post.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserXEntity } from '../auth-module/auth.entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { PostEntity } from './post.entity';
+import { ProfilePicEntity } from './profile.pic.entity';
+import { UserXEntity } from '../auth-module/auth.entity';
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(PostEntity)
     private postRepository: Repository<PostEntity>, // Inject Post repository
+
     @InjectRepository(UserXEntity)
-    private userRepository: Repository<UserXEntity>, // Inject UserXEntity repository (if needed)
+    private userXRepository: Repository<UserXEntity>, // Inject UserXEntity repository (if needed)
+
+    @InjectRepository(ProfilePicEntity)
+    private profilePicRepository: Repository<ProfilePicEntity>, // Inject Post repository
+
     private jwtService: JwtService // Inject JwtService
   ) {}
 
-  async getData() {
-    return 'shit';
+  async uploadProfilePic(filePath: string, userId: string) {
+    // Find the user by ID from the repository
+    const userX = await this.userXRepository.findOne({ where: { id: userId } });
+
+    if (!userX) {
+      throw new Error('User not found');
+    }
+
+    // Update the user's profile picture path
+    userX.filePath = filePath;
+
+    // Save the updated user entity
+    return await this.userXRepository.save(userX);
   }
 }
