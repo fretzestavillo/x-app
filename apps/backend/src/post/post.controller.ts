7fa93f50a202de3576118c1920d260fe70cb71ec
fretzestavillo <estavillofretz@gmail.com>
2025-02-18 14:45:16 +0800

    add multer

--- apps/backend/src/post/post.controller.ts
+++ apps/backend/src/post/post.controller.ts
@@ -1,13 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import 'multer';
+
 import { PostService } from './post.service';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller()
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Get('getPost')
-  async getData(data: any): Promise<any> {
-    const result = await this.postService.getData();
-    return result;
+  @Post('uploadProfilePic')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: any
+  ) {
+    Logger.log('Here at controller');
+
+    // Generate a unique filename to prevent overwriting
+    const filename = Date.now() + '-' + file.originalname;
+
+    // Define the file path (where to store the file)
+    const uploadDir = path.join(__dirname, '..', '..', '..', 'uploads'); // Going up one level from the current directory (which may be dist)
+
+    // Check if the 'uploads' directory exists, if not, create it
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    // Define the full file path
+    const filePath = path.join(uploadDir, filename);
+
+    // Save the file to disk
+    fs.writeFileSync(filePath, file.buffer);
+
+    // Optionally, save file path in the database
+    const fileEntity = await this.postService.uploadProfilePic(
+      filePath,
+      body.id
+    ); // Assuming FileService has a saveFile method
+
+    return {
+      message: 'Profile picture uploaded successfully!',
+      filePath: fileEntity.filePath, // Return file path or any necessary info
+    };
   }
 }
